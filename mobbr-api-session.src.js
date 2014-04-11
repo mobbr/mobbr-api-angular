1143ef0daeda8922e982e046702139ea062b2f4a
Han Dijk <info@handijk.nl>
2014-04-11 11:50:31 +0200

    let MobbrSession login and logout users

--- mobbr-api-session.src.js
+++ mobbr-api-session.src.js
@@ -39,15 +39,17 @@ angular.module('mobbrSession', [ 'mobbrApi' ]).factory('mobbrSession', function 
 }).config(function ($httpProvider) {
     $httpProvider.interceptors.push('mobbrSessionInterceptor');
 });
-angular.module('mobbrSession').factory('mobbrSessionInterceptor', function (mobbrConfig, mobbrSession) {
+angular.module('mobbrSession').factory('mobbrSessionInterceptor', function (mobbrConfig, mobbrSession, MobbrUser) {
 
     return {
         request: function (config) {
-            console.log(config);
             if (mobbrConfig.isApiUrl(config.url) && mobbrSession.isAuthorized()) {
                 config.headers.Authorization = mobbrSession.getAuthorization();
             }
             return config;
+        },
+        response: function (response) {
+            console.log(response, MobbrUser);
         },
         responseError: function (rejection) {
             if (mobbrConfig.isApiUrl(rejection.config.url) && rejection.status === 401) {
--- src/session/mobbr-session-interceptor.js
+++ src/session/mobbr-session-interceptor.js
@@ -1,12 +1,14 @@
-angular.module('mobbrSession').factory('mobbrSessionInterceptor', function (mobbrConfig, mobbrSession) {
+angular.module('mobbrSession').factory('mobbrSessionInterceptor', function (mobbrConfig, mobbrSession, MobbrUser) {
 
     return {
         request: function (config) {
-            console.log(config);
             if (mobbrConfig.isApiUrl(config.url) && mobbrSession.isAuthorized()) {
                 config.headers.Authorization = mobbrSession.getAuthorization();
             }
             return config;
+        },
+        response: function (response) {
+            console.log(response, MobbrUser);
         },
         responseError: function (rejection) {
             if (mobbrConfig.isApiUrl(rejection.config.url) && rejection.status === 401) {
