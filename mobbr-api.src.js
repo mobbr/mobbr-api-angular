fdf8f0d8ff09d9c3c0f6e1bff2bef0fb0aa9143a
Han Dijk <info@handijk.nl>
2014-08-21 12:41:56 +0200

    deletes

--- mobbr-api.src.js
+++ mobbr-api.src.js
@@ -264,9 +264,9 @@ angular.module('mobbrApi').factory('MobbrPayment', function ($resource, mobbrCon
             }
         },
         unclaimShares: {
-            method: 'POST',
+            method: 'DELETE',
             params: {
-                action: 'unclaim_shares'
+                action: 'unclaimed_shares'
             }
         },
         claim: {
