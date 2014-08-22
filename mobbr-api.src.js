5ef2ca06ec9fd0f84924dcc26b34988a54517894
Han Dijk <info@handijk.nl>
2014-08-22 16:05:16 +0200

    balances user is default get

--- mobbr-api.src.js
+++ mobbr-api.src.js
@@ -95,12 +95,6 @@ angular.module('mobbrApi').factory('MobbrApi', function ($resource, mobbrConfig)
 angular.module('mobbrApi').factory('MobbrBalance', function ($resource, mobbrConfig) {
 
     return $resource(mobbrConfig.url + 'balances/:action', {}, {
-        user: {
-            method: 'GET',
-            params : {
-                action: 'user'
-            }
-        },
         uri: {
             method: 'GET',
             params : {
