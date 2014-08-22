e241ddb61fe44f56f4cca8ae18312188677cd1db
Han Dijk <info@handijk.nl>
2014-08-22 16:09:54 +0200

    notifications user is default get and delete

--- src/api/mobbr-notifications.js
+++ src/api/mobbr-notifications.js
@@ -1,17 +1,4 @@
 angular.module('mobbrApi').factory('MobbrNotifications', function ($resource, mobbrConfig) {
 
-    return $resource(mobbrConfig.url + 'notifications/:action', {}, {
-        user: {
-            method: 'GET',
-            params : {
-                action: 'user'
-            }
-        },
-        delete: {
-            method: 'DELETE',
-            params : {
-                action: 'user'
-            }
-        }
-    });
+    return $resource(mobbrConfig.url + 'notifications/:action', {}, {});
 });
