8bf9e0b39c8df7ff95405dc4d9cb584e29e80dfb
Han Dijk <info@handijk.nl>
2014-09-17 09:50:17 +0200

    make invoices up to date with the api

--- src/api/mobbr-invoices.js
+++ src/api/mobbr-invoices.js
@@ -1,52 +1,11 @@
 angular.module('mobbrApi').factory('MobbrInvoice', function ($resource, mobbrConfig) {
 
     return $resource(mobbrConfig.url + 'invoices/:action', {}, {
-        requestable: {
+        get: {
             method: 'GET',
+            responseType: 'blob',
             params : {
                 action: 'requestable'
-            }
-        },
-        request: {
-            method: 'PUT',
-            params : {
-                action: 'request'
-            }
-        },
-        unrequest: {
-            method: 'PUT',
-            params : {
-                action: 'unrequest'
-            }
-        },
-        requested: {
-            method: 'GET',
-            params : {
-                action: 'requested'
-            }
-        },
-        confirmable: {
-            method: 'GET',
-            params : {
-                action: 'confirmable'
-            }
-        },
-        confirm: {
-            method: 'PUT',
-            params : {
-                action: 'confirm'
-            }
-        },
-        returned: {
-            method: 'GET',
-            params : {
-                action: 'returned'
-            }
-        },
-        confirmed: {
-            method: 'GET',
-            params : {
-                action: 'confirmed'
             }
         }
     });
